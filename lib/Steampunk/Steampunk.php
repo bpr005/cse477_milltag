6bc7d66b4290323466faf4bd98c1443a243fd307
merril51 <merril51@msu.edu>
2016-02-16 22:57:57 -0500

    add more model functionality, player1 and player2, saving the grid as an array of arrays.

--- lib/Steampunk/Steampunk.php
+++ lib/Steampunk/Steampunk.php
@@ -22,8 +22,20 @@ class Steampunk
 
     public function newGame($size) {
         $this->gameSize = $size;
+        $this->grid = array();
+
+        for ($x=0; $x < $size; $x++) {
+            $this->grid[] = array();
+
+        }
     }
 
+    public function setPlayer1($player1) {
+        $this->player1 = $player1;
+    }
+    public function setPlayer2($player2) {
+        $this->player2 = $player2;
+    }
     public function getCurrentPlayer() {
         return $this->currentPlayer;
     }
@@ -33,10 +45,29 @@ class Steampunk
         return $this->gameSize;
     }
 
+    public function getGridSize() {
+        return count($this->grid);
+    }
+
     public function newTurn($s){
         $this->currentPlayer = $s;
     }
 
+    public function addPipe($pipeType, $x, $y) {
+        $this->grid[$x][$y] = $pipeType;
+    }
+    public function getTileContents($x, $y) {
+        return $this->grid[$x][$y];
+
+    }
+
     private $gameSize; // BR: Cam, what is your plan for this variable? An integer since we only have square grids?
     private $currentPlayer;
+    private $player1;
+    private $player2;
+
+    const GAME_WIN = 1; //might need this for game winning?
+    private $grid = array(); //store the grid as an array of arrays
+
+
 }
--- tests/tests/SteampunkTest.php
+++ tests/tests/SteampunkTest.php
@@ -12,10 +12,30 @@ class SteampunkTest extends \PHPUnit_Framework_TestCase
 	public function test_construct() {
 		$steampunk = new Steampunk\Steampunk(self::SEED);
 		$this->assertInstanceOf("Steampunk\Steampunk", $steampunk);
-
-
 	}
+	public function test_check1() {
+		$steampunk = new Steampunk\Steampunk(self::SEED);
+		$steampunk->newGame(5);
+		$this->assertEquals(5, $steampunk->getGameSize());
+		$this->assertEquals(5, $steampunk->getGridSize());
+
+		$steampunk->newGame(6);
+		$this->assertEquals(6, $steampunk->getGameSize());
+		$this->assertEquals(6, $steampunk->getGridSize());
+
+
+		$steampunk->newTurn("Cam");
+		$this->assertEquals("Cam", $steampunk->getCurrentPlayer());
+
+		$steampunk->newTurn("Bonnie");
+		$this->assertEquals("Bonnie", $steampunk->getCurrentPlayer());
+
+		$steampunk->addPipe("tee-esw", 1,2);
+		$this->assertEquals("tee-esw", $steampunk->getTileContents(1,2));
+	}
+
 }
+
 
 /// @endcond
 ?>
